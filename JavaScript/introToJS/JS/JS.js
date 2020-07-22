253550aedd21666a21065bde43a904a1e9ff5b42
vinesh <vineshghela@gmail.com>
2020-07-22 12:05:35 +0100

    objects and classes added

--- JavaScript/introToJS/JS/JS.js
+++ JavaScript/introToJS/JS/JS.js
@@ -1,29 +1,81 @@
 
-// let car = new Object();
-// car["Make"] = "Audi";
-// car["Model"] ="A5";
-// car["CarReg"] = "W8M8";
 
-// console.log(car)
-// console.log(car.Make);
+// console.log(`the sum is :${letsAdd(10,25)}`);
 
-// car.numberOfDoors=4;
-// console.log(car);
+// function letsAdd(a,b){
+//     return a+b;
+// }
 
-// let car2 ={Make: "BMW", Model: "M5", CarReg: "W8M82", numberOfDoors: 4}
-// console.log(car2);
+// greeting();
 
-// Arrays
-let a = Array();
-let b= Array(10);
-let c = Array("Tom","Jim","Harry");
-let d = ["jim","bob","bam"];
-console.log(a);
-console.log(b);
-console.log(c);
-console.log(d);
+// function greeting(){
+//     console.log("Hello everyone")
+// }
 
-b[0] = "john";
-b[1] ="peter";
 
-console.log(b);
+
+// let hi = function(name){
+//     alert(`Well this is annoying ${name}`);
+// }
+
+// // hi();
+
+// // hi("Piers");
+// // hi("Alan");
+
+
+// hello = (name, age) =>"Hello" + name +" my age is"+ age;
+
+
+// console.log(hello("Jim",20));
+
+// bye = name => "Hello" +name;
+
+// console.log(bye("Vin"));
+
+
+// // [1,2,3].forEach(alert);
+
+// if(false){
+//     console.log("Hello")
+// }else{
+//     console.log("Good bye!")
+// }
+
+// let age1 =18;
+
+// let status = (age1>=18)? "Adult":"Minor";
+
+// console.log(status);
+
+//objects
+
+class pet{
+    constructor(petName){
+        this._name = petName;
+        this._eaten = false;
+    }
+    hungry(){
+        if(!this._eaten){
+            console.log(`${this._name} is hungry`)
+        }else{
+            console.log(`${this._name} has eaten!`)
+        }
+    }
+    eating(){
+        console.log(`${this._name} is eating!`)
+    }
+    speak(){
+        console.log(`${this._name} says hi!!!`)
+    }
+
+
+}
+
+let newPuppy = new pet("Sir bark-Alot!");
+newPuppy.eating();
+newPuppy.hungry();
+newPuppy.speak();
+
+
+
---
+++ JavaScript/introToJS/JS/js2.js
@@ -0,0 +1,21 @@
+class dog extends pet{
+    constructor(name, tricks) {
+        super(name); // Calls the super constructor and assigns the attribute.
+        this._tricks = tricks;
+      }
+      play() {
+        console.log(`${this._name} wants to show you his tricks!`);
+      }
+    
+      tricks() {
+        console.log(`${this._name} can do a ${this._tricks}`);
+      }
+      speak() {
+        super.speak();
+        console.log(`WOOOOOOOOOF`); // Overrides parent function call();
+      }
+    }
+    
+    let doggy = new dog("Bark Wahlberg", "Backflip");
+    doggy.hungry();
+    doggy.speak();
